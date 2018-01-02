2bdf6bbabdb4f1d642740cee77b62c77e960f517
Mohsen Hadianfard <mhadianfard@sweettoothhq.com>
2018-01-02 09:05:51 -0500

    Controller parameter to fetch all pages
    Needs to be refactored

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -7,55 +7,95 @@ class FetchController < ApplicationController
   SHOPIFY_APPSTORE_URL = 'https://apps.shopify.com/smile-io'
 
   def shopify
-    @reviews = []
-    shouldSave = true
-    accountLookup = true
+    shouldSave = params[:save] ||= ''
+    if shouldSave.downcase == 'false' || shouldSave == '0'
+      shouldSave = false
+    else
+      shouldSave = true
+    end
+
+    shouldLookupAccount = params[:account_lookup] ||= ''
+    if shouldLookupAccount.downcase == 'false' || shouldLookupAccount == '0'
+      shouldLookupAccount = false
+    else
+      shouldLookupAccount = true
+    end
+
     page = params[:page] ||= 1
-    fetchUrl = SHOPIFY_APPSTORE_URL + "?page=#{page}#reviews"
-    document = Nokogiri::HTML(open(fetchUrl))
+    if page == 'all'
+      reviews = []
+      currentPage = 1
+      while true do
+        logger.debug "Fetching page #{currentPage}"
+        reviewsOfCurrentPage = fetchPage(currentPage, shouldLookupAccount, shouldSave)
+        logger.debug "Got all reviews in page #{currentPage}"
+        reviewsOfCurrentPage.each do |review|
+          reviews.push(review)
+        end
+        logger.debug "Done storing reviews in page #{currentPage}"
+        currentPage += 1
 
-    reviewsSection = document.search('[id=reviews]')
-    reviewElements = reviewsSection.search('[itemprop=review]')
-
-    reviewElements.each do |reviewElement|
-      externalId = reviewElement['data-review_id']
-      review = Review.find_by_external_id(externalId)
-      if (review.nil?)
-        review = Review.new
-        review.external_id = externalId
-      end
-
-      review.appstore = APPSTORE_NAME
-
-      ratingElement = reviewElement.search('[itemprop=reviewRating]').last
-      review.rating = ratingElement['content']
-
-      datePublishedElement = reviewElement.search('[itemprop=datePublished]').last
-      review.published_at = DateTime.rfc3339(datePublishedElement['content'])
-
-      reviewBodyElement = reviewElement.search('[itemprop=reviewBody]').last
-      review.body = reviewBodyElement.text
-
-      authorElement = reviewElement.search('[itemprop=author]').last
-      review.shop_url = authorElement['href']
-      review.shop_name = authorElement.text
-
-      if (accountLookup && review.account_id.nil?)
-        channelExternalId = review.shop_url.sub('https://', '').sub('http://', '')
-        channel = Channel::Shopify.find_by_external_id(channelExternalId)
-        if (!channel.nil?)
-          review.account_id = channel.account_id
+        if reviewsOfCurrentPage.empty?
+          break;
         end
       end
 
-      if (shouldSave)
-        review.save
-      end
-
-      @reviews.push(review)
+    else
+      reviews = fetchPage(page, shouldLookupAccount, shouldSave)
     end
 
-    render json: @reviews
+    render json: reviews
   end
 
+
+  protected
+
+    def fetchPage (page, shouldLookupAccount, shouldSave)
+      reviews = []
+      fetchUrl = SHOPIFY_APPSTORE_URL + "?page=#{page}#reviews"
+      document = Nokogiri::HTML(open(fetchUrl))
+
+      reviewsSection = document.search('[id=reviews]')
+      reviewElements = reviewsSection.search('[itemprop=review]')
+
+      reviewElements.each do |reviewElement|
+        externalId = reviewElement['data-review_id']
+        review = Review.find_by_external_id(externalId)
+        if (review.nil?)
+          review = Review.new
+          review.external_id = externalId
+        end
+
+        review.appstore = APPSTORE_NAME
+
+        ratingElement = reviewElement.search('[itemprop=reviewRating]').last
+        review.rating = ratingElement['content']
+
+        datePublishedElement = reviewElement.search('[itemprop=datePublished]').last
+        review.published_at = DateTime.rfc3339(datePublishedElement['content'])
+
+        reviewBodyElement = reviewElement.search('[itemprop=reviewBody]').last
+        review.body = reviewBodyElement.text
+
+        authorElement = reviewElement.search('[itemprop=author]').last
+        review.shop_url = authorElement['href']
+        review.shop_name = authorElement.text
+
+        if shouldLookupAccount && review.account_id.nil?
+          channelExternalId = review.shop_url.sub('https://', '').sub('http://', '')
+          channel = Channel::Shopify.find_by_external_id(channelExternalId)
+          if !channel.nil?
+            review.account_id = channel.account_id
+          end
+        end
+
+        if shouldSave
+          review.save
+        end
+
+        reviews.push(review)
+      end
+
+      return reviews
+    end
 end
