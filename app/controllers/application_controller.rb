528e712666668a76f99d33a14b4eac74a92cc1a6
Mohsen Hadianfard <mhadianfard@sweettoothhq.com>
2018-01-02 11:17:05 -0500

    Adding basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV["auth_user"], password: ENV["auth_password"]
+ 
 end
